edccaf52877821194998534ed963b187128c1038
siddiqsid1995 <siddiqsid1995@gmail.com>
2025-11-19 18:19:20 +0530

    done

---
+++ src/org/test/Sample.java
@@ -0,0 +1,15 @@
+package org.test;
+
+public class Sample {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome");
+
+		int a = 10;
+
+		System.out.println(a / 2);
+
+	}
+
+}
