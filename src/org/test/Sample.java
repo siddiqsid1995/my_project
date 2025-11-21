f486ce237be6ea1664354d6dae60b50ab4301ce7
siddiqsid1995 <siddiqsid1995@gmail.com>
2025-11-21 18:16:36 +0530

    am done my code : Jeyanth

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -1,5 +1,8 @@
 package org.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sample {
 
 	public static void main(String[] args) {
@@ -9,6 +12,14 @@ public class Sample {
 		int a = 10;
 
 		System.out.println(a / 2);
+
+		List<Integer> li = new ArrayList<>();
+
+		li.add(20);
+		li.add(30);
+		li.add(40);
+
+		System.out.println(li);
 
 	}
 
